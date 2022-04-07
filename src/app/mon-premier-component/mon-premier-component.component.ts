3d50c743f12ebe1a6954d2590a4186495a9b2c3e
Arthur Delplace <easycash@pc21.home>
2022-04-07 17:27:42 +0200

    résolution ampoules

--- src/app/mon-premier-component/mon-premier-component.component.ts
+++ src/app/mon-premier-component/mon-premier-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input ,OnInit } from '@angular/core';
+import { Component, Input ,OnInit, } from '@angular/core';
 
 @Component({
   selector: 'app-mon-premier-component',
@@ -8,17 +8,50 @@ import { Component, Input ,OnInit } from '@angular/core';
 export class MonPremierComponentComponent implements OnInit {
   @Input() appareilName: string | undefined;
   @ Input() appareilFunction: string | undefined;
-  @ Input() appareilStatus = 'allumé';
-  @ Input() appareilOn = 'éteint';
+  @ Input() appareilStatus = 'éteint';
   // appareil = 'éteint';
+  @ Input() appareilStatus2 = 'éteint';
+  @ Input() appareilOn = '&&&';
+  @ Input() colorRed = '&&&';
+  // @ Input() colorGreen = 'Green';
+  
+
+  appareil = 'éteint';
   constructor() { }
 
   ngOnInit(): void {
   }
   getStatus(){
-    return this.appareilStatus;
+    return this.appareilStatus2;
   }
+
   getStatus2(){
     return this.appareilOn;
+    
   }
+
+  // getStatus3(){
+    
+  // }
+  
+  getColor(){
+   if (this.appareilStatus2 === 'allumé'){
+    return 'green'
+   } else if(this.appareilStatus2 === 'éteint') {
+     return 'red'
+   }
+    return 'blue';
+    // {
+    //   return 'red';
+    // }
+        
+    }
+    
+  // getColor2(){
+  //   if (this.appareilStatus2 === 'éteint'){
+
+  //   }
+  //   return this.colorRed;
+  // }
+  
 }
